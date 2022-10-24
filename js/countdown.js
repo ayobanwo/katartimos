04f77a66e83f8389fef7b87052d7a3add50f988e
ayobanwo <olubanwoayomide@gmail.com>
2022-10-23 17:11:43 -0700

    countdown

---
+++ js/countdown.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Unix timestamp (in seconds) to count down to
+  var twoDaysFromNow = ( 1666570043933 / 1000) + (86400 * 30) + 1;
+
+  // Set up FlipDown
+  var flipdown = new FlipDown(twoDaysFromNow)
+
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+      console.log('The countdown has ended!');
+    });
+
+
+let body = document.body;
+
+body.querySelector('#flipdown').classList.add('flipdown__theme-light');
+
+  var ver = document.getElementById('ver');
+  ver.innerHTML = flipdown.version;
+});
