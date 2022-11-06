fe53ea23b51aa190206d1f1dac0a178d012e353f
ayobanwo <olubanwoayomide@gmail.com>
2022-11-06 04:01:25 +0100

    backend integration

--- katarReg/script.js
+++ katarReg/script.js
@@ -1,49 +1,85 @@
 let nextBtn = document.querySelector('.nextBtn');
+let test = document.querySelector('.test');
 let form = document.querySelector('form');
+let membNo = document.querySelector('#membNo');
+let membYes = document.querySelector('#membYes');
+let nonMemb = document.querySelector('#nonMemb');
 
-nextBtn.addEventListener('click', e=>{
-    let fisrt_name = document.querySelector('#first_name');
-    let last_name = document.querySelector('#last_name');
-    let email = document.querySelector('#email');
-    let mobile_number = document.querySelector('#mobile_number');
-    let age_group = document.querySelector('#age_group');
-    let resident = document.querySelector('#resident');
-    let town = document.querySelector('#town');
-    let state = document.querySelector('#state');
-    let membNo = document.querySelector('#membNo');
-    let membYes = document.querySelector('#membYes');
+membNo.addEventListener('click', e=>{
+    if (membNo.checked === true) {
+        nonMemb.style.display = 'flex';
+    }
+    else{
+        nonMemb.style.display = 'none';
+    }
+})
 
-    if (membNo.checked) {
-        form.action = 'katarRegNM/index.html'
+membYes.addEventListener('click', e=>{
+    if (membYes.checked === true) {
+        nonMemb.style.display = 'none';
+    }
+})
+
+
+test.addEventListener('click', e=>{
+    e.preventDefault();
+    let first_name = document.querySelector('#first_name').value;
+    let last_name = document.querySelector('#last_name').value;
+    let email = document.querySelector('#email').value;
+    let mobile_number = document.querySelector('#mobile_number').value;
+    let gender = document.querySelector('#gender').value;
+    let age_group = document.querySelector('#age_group').value;
+    let resident = document.querySelector('#resident').value;
+    let town = document.querySelector('#town').value;
+    let state = document.querySelector('#state').value;
+    let knowledge = document.querySelector('#knowledge').value; 
+    let made_arrangement = document.querySelector('#made_arrangement').value; 
+    let expectation = document.querySelector('#expectation').value; 
+
+    
+    if(membNo.checked === true){
+        member = "not a member"
+    }
+    else if(membYes.checked === true){
+        member = "member"
     }
 
-    sessionStorage.setItem('first_name', first_name);
-    sessionStorage.setItem('last_name', last_name);
-    sessionStorage.setItem('email', email);
-    sessionStorage.setItem('mobile_number', mobile_number);
-    sessionStorage.setItem('age_group', age_group);
-    sessionStorage.setItem('resident', resident);
-    sessionStorage.setItem('state', state);
-    sessionStorage.setItem('town', town);
-})
-
-fetch("https://lightstream-app.herokuapp.com/api/new/form", {
-        method: 'post',
-        body: JSON.stringify(dataObj),
-        headers: {
-            'Content-Type': 'application/json'
+    let attend_Conference =  document.getElementsByName('attend_Conference'); 
+    let attended_before = ''
+    
+    attend_Conference.forEach( year => {
+        if (year.checked ) {
+            attended_before += " " + year.value ;
         }
-    }).then(function (response){
-        return response.json();
-    }).then(function (responseData){
-        let message = responseData.message;
-        if(message === 'Form succesfully created'){
-            console.log(responseData);
-            form.submit();
-        }
-      return message;
-    }).catch(function (error){
-        console.log(error);
     })
 
+    attended_before = attended_before.replace('undefined', '');
+
+    
+    dataObj = {first_name, last_name, email, mobile_number, gender, age_group, resident, state, town, member, knowledge, made_arrangement, expectation, attended_before }
+
+console.log(dataObj);
+
+    fetch("https://whitefieldmissions.org/backends/api/post/reg_adult.php", {
+            method: 'post',
+            body: JSON.stringify(dataObj),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function (response){
+            return response.json();
+        }).then(function (responseData){
+            let status = responseData.status;
+            let message = responseData.message;
+            console.log(status, message);
+            if(status === 'success'){
+                console.log(responseData);
+                form.submit();
+            }
+          return status;
+        }).catch(function (error){
+            console.log(error);
+        })
 })
+
+
---
+++ metamorphoorReg/script.js
@@ -0,0 +1,82 @@
+let submitBtn = document.querySelector('.submitBtn');
+let form = document.querySelector('form');
+
+submitBtn.addEventListener('click', e=>{
+    let leader_name = document.querySelector('#leader_name');
+    let leader_mobile_number = document.querySelector('#leader_mobile_number');
+    let pastor_name = document.querySelector('#pastor_name');
+    let first_name = document.querySelector('#first_name');
+    let other_name = document.querySelector('#other_name');
+    let last_name = document.querySelector('#last_name');
+    let gender = document.querySelector('#gender');
+    let dob = document.querySelector('#dob');
+    let age_group = document.querySelector('#age_group');
+    let school = document.querySelector('#school');
+    let parent = document.querySelector('#parent');
+    let parent_mobile = document.querySelector('#parent_mobile');
+    let resident = document.querySelector('#resident');
+    let town = document.querySelector('#town');
+    let state = document.querySelector('#state');
+    let parent_condition = document.querySelector('#parent_condition');
+
+    //Leader Church
+    let leader_churchArr =  document.getElementsByName('leader_church'); 
+    let leader_church;
+
+    leader_churchArr.forEach( church => {
+        if (church.checked ) {
+           leader_church = church.value ;
+        }
+    })
+
+    //Class Group
+    let class_groupArr =  document.getElementsByName('class_group'); 
+    let class_group
+    class_groupArr.forEach( group => {
+        if (group.checked ) {
+           class_group = group.value ;
+        }
+    })
+
+    //Parent Realationship
+    let relationshipArr =  document.getElementsByName('relationship'); 
+    let relationship 
+    relationshipArr.forEach( relation => {
+        if (relation.checked ) {
+           relationship = relation.value ;
+        }
+    })
+
+    //Parent Approval
+    let approvalArr =  document.getElementsByName('approval'); 
+    let approval
+    approvalArr.forEach( verdict => {
+        if (verdict.checked ) {
+           approval = verdict.value ;
+        }
+    })
+
+    dataObj = {leader_name, leader_church, first_name, other_name, leader_mobile_number, last_name, mobile_number, gender, age_group, class_group, resident, town, state, dob, school, parent, }
+    dataObj = {leader_name, leader_church, first_name, other_name, leader_mobile_number, last_name, mobile_number, gender, age_group, class_group, resident, town, state, dob, school, parent, relationship, parent_mobile, approval, parent_condition}
+
+    fetch("https://whitefieldmissions.org/backends/api/post/reg_child.php", {
+            method: 'post',
+            body: JSON.stringify(dataObj),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function (response){
+            return response.json();
+        }).then(function (responseData){
+           let status = responseData.status;
+            let message = responseData.message;
+            console.log(status, message);
+            if(status === 'success'){
+                console.log(responseData);
+                form.submit();
+            }
+          return status;
+        }).catch(function (error){
+            console.log(error);
+        })
+})
