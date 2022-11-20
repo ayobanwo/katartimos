b07d5aa8e6e0278edcb5c047ebec3f4edb23c77d
ayobanwo <olubanwoayomide@gmail.com>
2022-11-20 12:07:04 +0100

    ux tweaks

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // Unix timestamp (in seconds) to count down to
-  var twoDaysFromNow = ( 1666570043933 / 1000) + (86400 * 30) + 1;
+  var twoDaysFromNow = ( 1666570043933 / 1000) + (86400 * 41) + 1;
 
   // Set up FlipDown
   var flipdown = new FlipDown(twoDaysFromNow)
